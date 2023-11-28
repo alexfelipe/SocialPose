c727b4c1bc89fd1748368fadb7e5a64234302267
Alex Felipe <alexfelipevieira@gmail.com>
2023-11-28 13:47:34 -0300

    adding page

--- app/src/main/java/br/com/alexf/socialpose/features/feed/FeedScreen.kt
+++ app/src/main/java/br/com/alexf/socialpose/features/feed/FeedScreen.kt
@@ -1,7 +1,10 @@
 package br.com.alexf.socialpose.features.feed
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.VerticalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,31 +17,35 @@ import br.com.alexf.socialpose.samples.generateUrlImage
 import br.com.alexf.socialpose.ui.theme.SocialPoseTheme
 import kotlin.random.Random
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun FeedScreen() {
-    LazyColumn(Modifier.fillMaxSize()) {
-        items(10) {
-            var isLiked by rememberSaveable {
-                mutableStateOf(false)
-            }
-            val title = rememberSaveable {
-                generateLorem(Random.nextInt(2, 10))
-            }
-            val likes = rememberSaveable {
-                "${Random.nextInt(2, 1000)}"
-            }
-            val imageUrl = rememberSaveable {
-                generateUrlImage()
-            }
-            PostItem(
-                imageUrl = imageUrl,
-                title = title,
-                likes = likes,
-                isLiked = isLiked,
-                onLikeClick = { isLiked = !isLiked },
-                Modifier.fillParentMaxHeight()
-            )
+    val pageState = rememberPagerState(pageCount = {
+        10
+    })
+    VerticalPager(
+        state = pageState,
+        Modifier.fillMaxSize()
+    ) { page ->
+        var isLiked by rememberSaveable {
+            mutableStateOf(false)
         }
+        val title = rememberSaveable {
+            generateLorem(Random.nextInt(2, 10))
+        }
+        val likes = rememberSaveable {
+            "${Random.nextInt(2, 1000)}"
+        }
+        val imageUrl = rememberSaveable {
+            generateUrlImage()
+        }
+        PostItem(
+            imageUrl = imageUrl,
+            title = title,
+            likes = likes,
+            isLiked = isLiked,
+            onLikeClick = { isLiked = !isLiked },
+        )
     }
 }
 
